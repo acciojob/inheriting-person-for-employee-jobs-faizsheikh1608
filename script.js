c75839353d8c6b05b3467983bcfb5b9fc5620049
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-07-24 21:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Person {
   }
 
   greet() {
-    console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old.");
+    console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old.");
   }
 }
 
@@ -19,8 +19,6 @@ class Employee extends Person {
     console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
   }
 }
-let person = new Person("Alice", 25);
-person.greet();
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
