b68d7e915bba3324769b556c083a7a803398b79e
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-07-24 20:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // complete this js code
-function Person(name, age) {}
+class Person{
+	constructor(name,age){
+		this._name = name;
+		this._age = age;
+	}
 
-function Employee(name, age, jobTitle) {}
+	greet(){
+		console.log("Hello, my name is " + this._name + ", I am" + this._age + "years old.")
+	}
+}
+
+class Employee extends Person{
+	constructor(name,age,jobTittle){
+		super(name,age);
+		this._jobTittle = jobTittle;
+	}
+
+	jobGreet(){
+		console.log('Hello, my name is ' + this._name + ", I am " + this._age + " years old, and my job tittle is " + this._jobTittle);
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
